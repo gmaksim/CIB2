2e7eceb6893af0feac0ef0ab0220a6531070068c
gmaksim <gmaksim@users.noreply.github.com>
2016-12-12 15:57:53 +0300

    start test varint

---
+++ NlgDBcredProg/NlgDBcredProg/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace NlgDBcredProg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        { //need usersdb DB and Users TABLES
+            InitializeComponent();
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=usersdb;Integrated Security=True";
+            string sql = "SELECT * FROM Users";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                // Создаем объект DataAdapter
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                // Создаем объект Dataset
+                DataSet ds = new DataSet();
+                // Заполняем Dataset
+                adapter.Fill(ds);
+                // Отображаем данные
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
