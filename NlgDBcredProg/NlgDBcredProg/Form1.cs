15c838ba29d6cfc36fbf383fd28e345fac82fca8
gmaksim <gmaksim@users.noreply.github.com>
2016-12-12 17:13:29 +0300

    two datagrid

--- NlgDBcredProg/NlgDBcredProg/Form1.cs
+++ NlgDBcredProg/NlgDBcredProg/Form1.cs
@@ -12,7 +12,11 @@ using System.Data.SqlClient;
 namespace NlgDBcredProg
 {
     public partial class Form1 : Form
+
     {
+        private int nc;
+        private int nr;
+
         public Form1()
         { //need usersdb DB and Users TABLES
             InitializeComponent();
@@ -29,6 +33,24 @@ namespace NlgDBcredProg
                 adapter.Fill(ds);
                 // Отображаем данные
                 dataGridView1.DataSource = ds.Tables[0];
+            
+
+            }
+
+            string sql2 = "SELECT * FROM Files";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                // Создаем объект DataAdapter
+                SqlDataAdapter adapter = new SqlDataAdapter(sql2, connection);
+                // Создаем объект Dataset
+                DataSet ds = new DataSet();
+                // Заполняем Dataset
+                adapter.Fill(ds);
+                // Отображаем данные
+          
+                dataGridView2.DataSource = ds.Tables[0];
+
             }
         }
 
@@ -36,6 +58,33 @@ namespace NlgDBcredProg
         {
 
         }
+
+
+
+ 
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            nc = e.ColumnIndex;
+            nr = e.RowIndex;
+            dataGridView2.Rows[nr].Cells[nc].Selected = true;
+        }
+
+
+
+
+        /*  private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+          {
+              label1.Text = dataGridView2.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+          }
+
+
+          private void label1_Changed(object sender, EventArgs e)
+          {
+              this.ИМЯBindingSource.Filter = string.Format("CONVERT(СТОЛБЕЦ, 'System.String') LIKE '{0}'", this.label1.Text);
+          }*/
+
+
     }
 }
 
