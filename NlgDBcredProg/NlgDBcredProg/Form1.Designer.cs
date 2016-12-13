2b62a23acae65345286cd8d962089aa9fc8ea241
gmaksim <gmaksim@users.noreply.github.com>
2016-12-13 16:57:04 +0300

    fixed position of buttons, datagrids and etc. (working)

--- NlgDBcredProg/NlgDBcredProg/Form1.Designer.cs
+++ NlgDBcredProg/NlgDBcredProg/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(429, 239);
+            this.button2.Location = new System.Drawing.Point(12, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(132, 23);
             this.button2.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(429, 268);
+            this.button3.Location = new System.Drawing.Point(12, 41);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(132, 23);
             this.button3.TabIndex = 1;
--- NlgDBcredProg/NlgDBcredProg/Form1.cs
+++ NlgDBcredProg/NlgDBcredProg/Form1.cs
@@ -19,32 +19,31 @@ namespace NlgDBcredProg
         DataGridView gridFiles;
 
         public Form1()
-
+        
         {
-            InitializeComponent(); //this.Size = new Size(1000, 1000);
-            connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True");
-            adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection);
+            InitializeComponent();
+            Size = new Size(450, 450); //РАЗМЕР ДАТАГРИДОВ
+            connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"); //измени дома инстанс для синхронизации БД
+            adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection);                                       //инстансы - home-sqlexpress, work - cibexpress
             adapterFiles = new SqlDataAdapter("SELECT * FROM Files", connection);
-            dataSet = new DataSet();
+            dataSet = new DataSet(); 
             adapterUsers.Fill(dataSet, "Users");
             adapterFiles.Fill(dataSet, "Files");
             dataSet.Relations.Add("users-files", //название связи
             dataSet.Tables["Users"].Columns["Id"],//первичный ключ главной таблицы
             dataSet.Tables["Files"].Columns["UsersId"]);//внешний ключ подчиненной таблицы
             bindingSourceUsers = new BindingSource(dataSet, "Users");
-            bindingSourceFiles = new BindingSource(bindingSourceUsers, "users-files");
+            bindingSourceFiles = new BindingSource(bindingSourceUsers, "users-files"); 
             gridUsers = new DataGridView();
             gridUsers.Size = new Size(this.ClientRectangle.Width - 20, (this.ClientRectangle.Height >> 1) - 15);
-            gridUsers.Location = new Point(10, 10);
+            gridUsers.Location = new Point(170, 10); // ТУТ МЕНЯЕТСЯ ПОЗИЦИЯ ОТ ФОРМЫ!!!
             gridUsers.DataSource = bindingSourceUsers;
             gridFiles = new DataGridView();
             gridFiles.Size = gridUsers.Size;
-            gridFiles.Location = new Point(10, gridUsers.Bottom + 10);
+            gridFiles.Location = new Point(170, gridUsers.Bottom + 10); // И ТУТ
             gridFiles.DataSource = bindingSourceFiles;
             this.Controls.AddRange(new Control[] { gridUsers, gridFiles });
             dataSet.Tables["Users"].Columns["Id"].ReadOnly = true; // R/O на ид в юзер таблице 
-            
-
         }
 
         private void addButton_Click(object sender, EventArgs e)
@@ -52,12 +51,6 @@ namespace NlgDBcredProg
             DataRow row = dataSet.Tables[0].NewRow(); // добавляем новую строку в DataTable
             dataSet.Tables[0].Rows.Add(row);
         }
-
-
-
-        
-
-
 
         private void saveButton_Click(object sender, EventArgs e)
         {
@@ -70,40 +63,20 @@ namespace NlgDBcredProg
                 adapterUsers.InsertCommand.CommandType = CommandType.StoredProcedure;
                 adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@name", SqlDbType.NText, 10, "Name"));
                 adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@age", SqlDbType.NText, 10,"Age"));
-                SqlParameter parameter = adapterUsers.InsertCommand.Parameters.Add("@Id", SqlDbType.NChar, 10, "Id");
+                SqlParameter parameter = adapterUsers.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 10,"Id");
                 parameter.Direction = ParameterDirection.Output;
-
                 adapterUsers.Update(dataSet.Tables["Users"]);
             }
         }
 
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            StartPosition = FormStartPosition.CenterScreen;
+            this.Left += 400;
+            Size = new Size(1000, 700); //размер основной формы и позиция
+            
         }
 
-        
     }
  }
-
-//примечания:
-// students = files, groups = users
-//home-sqlexpress, work - cibexpress
-
-
-/*   //создание хранимой процедуры в MSSQL
-CREATE PROCEDURE [dbo].[sp_Users]
-    @name ntext,
-    @age ntext,
-	@Id nchar(10) output
-AS
-    INSERT INTO Users (Name, Age)
-    VALUES (@name, @age)
- 
-    SET @Id=SCOPE_IDENTITY()
-GO
-*/
-
-
 
