85e252a877a6d1f4a5ed15b57f12fc01700e5269
gmaksim <gmaksim@users.noreply.github.com>
2016-12-13 15:31:51 +0300

    two tables + add and save to DB button + sql procedure

--- NlgDBcredProg/NlgDBcredProg/Form1.Designer.cs
+++ NlgDBcredProg/NlgDBcredProg/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(429, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Добавить строку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(429, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Сохранить в БД";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(820, 649);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +67,10 @@
         }
 
         #endregion
+
+        protected internal System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- NlgDBcredProg/NlgDBcredProg/Form1.cs
+++ NlgDBcredProg/NlgDBcredProg/Form1.cs
@@ -12,7 +12,6 @@ namespace NlgDBcredProg
         SqlConnection connection;
         SqlDataAdapter adapterUsers;
         SqlDataAdapter adapterFiles;
-        // students = files, groups = users
         DataSet dataSet;
         BindingSource bindingSourceUsers;
         BindingSource bindingSourceFiles;
@@ -21,22 +20,17 @@ namespace NlgDBcredProg
 
         public Form1()
 
-
-            
-
         {
-
-            InitializeComponent();
-            //this.Size = new Size(1000, 1000);
-            connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=usersdb;Integrated Security=True");
+            InitializeComponent(); //this.Size = new Size(1000, 1000);
+            connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True");
             adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection);
             adapterFiles = new SqlDataAdapter("SELECT * FROM Files", connection);
             dataSet = new DataSet();
             adapterUsers.Fill(dataSet, "Users");
             adapterFiles.Fill(dataSet, "Files");
             dataSet.Relations.Add("users-files", //название связи
-                dataSet.Tables["Users"].Columns["Id"],//первичный ключ главной таблицы
-                dataSet.Tables["Files"].Columns["UsersId"]);//внешний ключ подчиненной таблицы
+            dataSet.Tables["Users"].Columns["Id"],//первичный ключ главной таблицы
+            dataSet.Tables["Files"].Columns["UsersId"]);//внешний ключ подчиненной таблицы
             bindingSourceUsers = new BindingSource(dataSet, "Users");
             bindingSourceFiles = new BindingSource(bindingSourceUsers, "users-files");
             gridUsers = new DataGridView();
@@ -48,22 +42,68 @@ namespace NlgDBcredProg
             gridFiles.Location = new Point(10, gridUsers.Bottom + 10);
             gridFiles.DataSource = bindingSourceFiles;
             this.Controls.AddRange(new Control[] { gridUsers, gridFiles });
-
+            dataSet.Tables["Users"].Columns["Id"].ReadOnly = true; // R/O на ид в юзер таблице 
+            
 
         }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            DataRow row = dataSet.Tables[0].NewRow(); // добавляем новую строку в DataTable
+            dataSet.Tables[0].Rows.Add(row);
+        }
+
+
+
+        
+
+
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
+            {
+                connection.Open();
+                adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapterUsers);
+                adapterUsers.InsertCommand = new SqlCommand("sp_Users", connection);
+                adapterUsers.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@name", SqlDbType.NText, 10, "Name"));
+                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@age", SqlDbType.NText, 10,"Age"));
+                SqlParameter parameter = adapterUsers.InsertCommand.Parameters.Add("@Id", SqlDbType.NChar, 10, "Id");
+                parameter.Direction = ParameterDirection.Output;
+
+                adapterUsers.Update(dataSet.Tables["Users"]);
+            }
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-
+        
     }
+ }
 
-       
+//примечания:
+// students = files, groups = users
+//home-sqlexpress, work - cibexpress
 
 
+/*   //создание хранимой процедуры в MSSQL
+CREATE PROCEDURE [dbo].[sp_Users]
+    @name ntext,
+    @age ntext,
+	@Id nchar(10) output
+AS
+    INSERT INTO Users (Name, Age)
+    VALUES (@name, @age)
+ 
+    SET @Id=SCOPE_IDENTITY()
+GO
+*/
 
-    }
 
 
