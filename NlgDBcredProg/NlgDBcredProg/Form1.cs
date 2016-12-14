9d9142114ea3c72e98a5681d1ceaa9cfdc228184
gmaksim <gmaksim@users.noreply.github.com>
2016-12-14 11:30:57 +0300

    with 3 dg

--- NlgDBcredProg/NlgDBcredProg/Form1.cs
+++ NlgDBcredProg/NlgDBcredProg/Form1.cs
@@ -10,13 +10,19 @@ namespace NlgDBcredProg
 
     {
         SqlConnection connection;
+        DataSet dataSet;
+        DataSet dataSet2;
         SqlDataAdapter adapterUsers;
         SqlDataAdapter adapterFiles;
-        DataSet dataSet;
+        SqlDataAdapter adapterOOO;
         BindingSource bindingSourceUsers;
         BindingSource bindingSourceFiles;
+        BindingSource bindingSourceOOO;
         DataGridView gridUsers;
         DataGridView gridFiles;
+        DataGridView gridOOO;
+        
+        
 
         public Form1()
         
@@ -26,33 +32,58 @@ namespace NlgDBcredProg
             connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"); //измени дома инстанс для синхронизации БД
             adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection);                                       //инстансы - home-sqlexpress, work - cibexpress
             adapterFiles = new SqlDataAdapter("SELECT * FROM Files", connection);
+            adapterOOO = new SqlDataAdapter("SELECT * FROM OOO", connection);
+
             dataSet = new DataSet(); 
             adapterUsers.Fill(dataSet, "Users");
             adapterFiles.Fill(dataSet, "Files");
+            adapterOOO.Fill(dataSet, "OOO");
+
             dataSet.Relations.Add("users-files", //название связи
-            dataSet.Tables["Users"].Columns["Id"],//первичный ключ главной таблицы
+            dataSet.Tables["Users"].Columns["FILESid"],//первичный ключ главной таблицы
             dataSet.Tables["Files"].Columns["UsersId"]);//внешний ключ подчиненной таблицы
             bindingSourceUsers = new BindingSource(dataSet, "Users");
-            bindingSourceFiles = new BindingSource(bindingSourceUsers, "users-files"); 
-            gridUsers = new DataGridView();
-            gridUsers.Size = new Size(this.ClientRectangle.Width - 20, (this.ClientRectangle.Height >> 1) - 15);
-            gridUsers.Location = new Point(170, 10); // ТУТ МЕНЯЕТСЯ ПОЗИЦИЯ ОТ ФОРМЫ!!!
-            gridUsers.DataSource = bindingSourceUsers;
-            gridFiles = new DataGridView();
-            gridFiles.Size = gridUsers.Size;
-            gridFiles.Location = new Point(170, gridUsers.Bottom + 10); // И ТУТ
-            gridFiles.DataSource = bindingSourceFiles;
-            this.Controls.AddRange(new Control[] { gridUsers, gridFiles });
-            dataSet.Tables["Users"].Columns["Id"].ReadOnly = true; // R/O на ид в юзер таблице 
-        }
+            bindingSourceFiles = new BindingSource(bindingSourceUsers, "users-files");
+          
 
+            dataSet.Relations.Add("ooo-users", //название связи
+            dataSet.Tables["OOO"].Columns["IdOOO"],//первичный ключ главной таблицы
+            dataSet.Tables["Users"].Columns["OOOid"]);//внешний ключ подчиненной таблицы
+            bindingSourceOOO = new BindingSource(dataSet, "OOO");
+            //bindingSourceUsers = new BindingSource(bindingSourceOOO, "ooo-users");
+            
+
+            //рисовка формы
+            gridUsers = new DataGridView(); //форма Users
+            gridUsers.Size = new Size(this.ClientRectangle.Width - 20, (this.ClientRectangle.Height >> 1) - 15);
+            gridUsers.Location = new Point(170, 10); 
+            gridUsers.DataSource = bindingSourceUsers;
+
+            gridFiles = new DataGridView(); //форма Files
+            gridFiles.Size = gridUsers.Size;
+            gridFiles.Location = new Point(170, gridUsers.Bottom + 10); 
+            gridFiles.DataSource = bindingSourceFiles;
+
+            gridOOO = new DataGridView(); //форма OOO
+            gridUsers.Size = new Size(this.ClientRectangle.Width - 20, (this.ClientRectangle.Height >> 1) - 15);
+            gridOOO.Location = new Point(20, gridFiles.Bottom + 10); 
+            gridOOO.DataSource = bindingSourceOOO;
+
+            this.Controls.AddRange(new Control[] { gridUsers, gridFiles, gridOOO });
+
+            dataSet.Tables["Users"].Columns["Id"].ReadOnly = true; // R/O на ид в юзер таблице 
+
+
+
+        }
+//кнопки для грида ФИО
         private void addButton_Click(object sender, EventArgs e)
         {
             DataRow row = dataSet.Tables[0].NewRow(); // добавляем новую строку в DataTable
             dataSet.Tables[0].Rows.Add(row);
         }
 
-        private void saveButton_Click(object sender, EventArgs e)
+        private void saveButton_Click(object sender, EventArgs e) 
         {
             using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
             {
