63eed27325ed4a7aeb92393d6d4f0fddcf69ecf0
gmaksim <gmaksim@users.noreply.github.com>
2016-12-16 15:33:07 +0300

    added LoanAgr tables and fixed position of 4 dg

--- NlgDBcredProg/NlgDBcredProg/Form1.cs
+++ NlgDBcredProg/NlgDBcredProg/Form1.cs
@@ -11,16 +11,10 @@ namespace NlgDBcredProg
     {
         SqlConnection connection;
         DataSet dataSet;
-        SqlDataAdapter adapterUsers;
-        SqlDataAdapter adapterFiles;
-        SqlDataAdapter adapterOOO;
-        BindingSource bindingSourceUsers;
-        BindingSource bindingSourceFiles;
-        BindingSource bindingSourceOOO;
-        DataGridView gridUsers;
-        DataGridView gridFiles;
-        DataGridView gridOOO;
-        
+        SqlDataAdapter adapterUsers, adapterFiles, adapterOOO, adapterLoan;
+        BindingSource bindingSourceUsers, bindingSourceFiles, bindingSourceOOO, bindingSourceLoan;
+        DataGridView gridUsers, gridFiles, gridOOO, gridLoan;
+
         public Form1()
         
         {
@@ -30,49 +24,52 @@ namespace NlgDBcredProg
             adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection); //select from tables
             adapterFiles = new SqlDataAdapter("SELECT * FROM Files", connection);
             adapterOOO = new SqlDataAdapter("SELECT * FROM OOO", connection);
-
+            adapterLoan = new SqlDataAdapter("SELECT * FROM LoanAgr", connection);
             dataSet = new DataSet(); //create dataset with tables
             adapterUsers.Fill(dataSet, "Users");
             adapterFiles.Fill(dataSet, "Files");
             adapterOOO.Fill(dataSet, "OOO");
-
-            dataSet.Relations.Add("ooo-users", //relations OOO and Users      
+            adapterLoan.Fill(dataSet, "LoanAgr");
+            dataSet.Relations.Add("ooo-loanagr", //relations OOO and LoanAgr      
             dataSet.Tables["OOO"].Columns["IdOOO"],             
-            dataSet.Tables["Users"].Columns["OOOid"]);         
-
+            dataSet.Tables["LoanAgr"].Columns["LoanId"]);
+            dataSet.Relations.Add("loanagr-users", //relations LoanAgr and Users      
+            dataSet.Tables["LoanAgr"].Columns["Id"],
+            dataSet.Tables["Users"].Columns["IdUsers"]);
             dataSet.Relations.Add("users-files", //relations Users and Files
             dataSet.Tables["Users"].Columns["FILESid"],
             dataSet.Tables["Files"].Columns["UsersId"]);
-
-
-            bindingSourceOOO = new BindingSource(dataSet, "OOO"); //bs dataset
+            bindingSourceLoan = new BindingSource(dataSet, "LoanAgr"); //bs dataset
+            bindingSourceOOO = new BindingSource(dataSet, "OOO"); 
             bindingSourceUsers = new BindingSource(dataSet, "Users");
-
-            bindingSourceUsers = new BindingSource(bindingSourceOOO, "ooo-users"); //bs with relations
+            bindingSourceLoan = new BindingSource(bindingSourceOOO, "ooo-loanagr"); //bs with relations
+            bindingSourceUsers = new BindingSource(bindingSourceLoan, "loanagr-users"); 
             bindingSourceFiles = new BindingSource(bindingSourceUsers, "users-files");
-
+            gridOOO = new DataGridView(); //dg OOO
+            gridOOO.Size = new Size(300, 610);
+            gridOOO.Location = new Point(5, 5);
+            gridOOO.DataSource = bindingSourceOOO;
+            gridLoan = new DataGridView(); //dg LoanAgr
+            gridLoan.Size = new Size(670, 150);
+            gridLoan.Location = new Point(310, 5);
+            gridLoan.DataSource = bindingSourceLoan;
             gridUsers = new DataGridView(); //dg Users
-            gridUsers.Size = new Size(670, 380);
-            gridUsers.Location = new Point(310, 5); 
+            gridUsers.Size = new Size(670, 200);
+            gridUsers.Location = new Point(310, gridLoan.Bottom + 30);
             gridUsers.DataSource = bindingSourceUsers;
-
             gridFiles = new DataGridView(); //dg Files
             gridFiles.Size = new Size(670, 200);
             gridFiles.Location = new Point(310, gridUsers.Bottom + 30); 
             gridFiles.DataSource = bindingSourceFiles;
+            this.Controls.AddRange(new Control[] { gridUsers, gridFiles, gridOOO, gridLoan }); //control with 4 dg
 
-            gridOOO = new DataGridView(); //dg OOO
-            gridOOO.Size = new Size (300, 610);
-            gridOOO.Location = new Point(5, 5);
-            gridOOO.DataSource = bindingSourceOOO;
-
-            this.Controls.AddRange(new Control[] { gridUsers, gridFiles, gridOOO }); //control with 3 dg
-            dataSet.Tables["Users"].Columns["Id"].ReadOnly = true; // R/O for 3 id in tables (temporary security)
-            dataSet.Tables["OOO"].Columns["IdOOO"].ReadOnly = true;
-            dataSet.Tables["Files"].Columns["UsersId"].ReadOnly = true;
+    //      dataSet.Tables["Users"].Columns["Id"].ReadOnly = true; // R/O for 3 id in tables (temporary security, in future hide in SELECT query)
+    //      dataSet.Tables["OOO"].Columns["IdOOO"].ReadOnly = true;
+    //      dataSet.Tables["Files"].Columns["UsersId"].ReadOnly = true;
+    //      dataSet.Tables["Files"].Columns["UsersId"].ReadOnly = true;
         }
 
-        private void saveButtonOOO_Click(object sender, EventArgs e) //save for OOO in DB 
+        private void saveButtonOOO_Click(object sender, EventArgs e) //save for OOO in DB - TO-DO
         {
             using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
             {
@@ -88,7 +85,7 @@ namespace NlgDBcredProg
                 adapterUsers.Update(dataSet.Tables["Users"]);
             }
         }
-        private void saveButtonUsers_Click(object sender, EventArgs e) //save for Users in DB 
+        private void saveButtonUsers_Click(object sender, EventArgs e) //save for Users in DB - WORK
         {
             using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
             {
@@ -104,7 +101,23 @@ namespace NlgDBcredProg
                 adapterUsers.Update(dataSet.Tables["Users"]);
             }
         }
-        private void saveButtonFiles_Click(object sender, EventArgs e) //save for OOO in DB 
+        private void saveButtonFiles_Click(object sender, EventArgs e) //save for Files in DB - TO-DO
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
+            {
+                connection.Open();
+                adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapterUsers);
+                adapterUsers.InsertCommand = new SqlCommand("sp_Users", connection);
+                adapterUsers.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@name", SqlDbType.NText, 10, "Name"));
+                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@age", SqlDbType.NText, 10, "Age"));
+                SqlParameter parameter = adapterUsers.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 10, "Id");
+                parameter.Direction = ParameterDirection.Output;
+                adapterUsers.Update(dataSet.Tables["Users"]);
+            }
+        }
+        private void saveButtonLoan_Click(object sender, EventArgs e) //save for LoanAgr in DB - TO-DO
         {
             using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
             {
@@ -130,4 +143,3 @@ namespace NlgDBcredProg
 
     }
  }
-
