0c7852d48dd6000f168fad19db9cb026f3d73d2f
gmaksim <gmaksim@users.noreply.github.com>
2016-12-20 17:46:32 +0300

    3 save buttons, 4 data grid, hidden cells

--- NlgDBcredProg/NlgDBcredProg/Form1.Designer.cs
+++ NlgDBcredProg/NlgDBcredProg/Form1.Designer.cs
@@ -66,7 +66,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(315, 158);
+            this.button5.Location = new System.Drawing.Point(315, 208);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(183, 23);
             this.button5.TabIndex = 3;
--- NlgDBcredProg/NlgDBcredProg/Form1.cs
+++ NlgDBcredProg/NlgDBcredProg/Form1.cs
@@ -25,11 +25,13 @@ namespace NlgDBcredProg
             adapterFiles = new SqlDataAdapter("SELECT * FROM Files", connection);
             adapterOOO = new SqlDataAdapter("SELECT * FROM OOO", connection);
             adapterLoan = new SqlDataAdapter("SELECT * FROM LoanAgr", connection);
+
             dataSet = new DataSet(); //create dataset with tables
             adapterUsers.Fill(dataSet, "Users");
             adapterFiles.Fill(dataSet, "Files");
             adapterOOO.Fill(dataSet, "OOO");
             adapterLoan.Fill(dataSet, "LoanAgr");
+
             dataSet.Relations.Add("ooo-loanagr", //relations OOO and LoanAgr      
             dataSet.Tables["OOO"].Columns["IdOOO"],             
             dataSet.Tables["LoanAgr"].Columns["LoanId"]);
@@ -39,36 +41,75 @@ namespace NlgDBcredProg
             dataSet.Relations.Add("users-files", //relations Users and Files
             dataSet.Tables["Users"].Columns["FILESid"],
             dataSet.Tables["Files"].Columns["UsersId"]);
+
             bindingSourceLoan = new BindingSource(dataSet, "LoanAgr"); //bs dataset
             bindingSourceOOO = new BindingSource(dataSet, "OOO"); 
             bindingSourceUsers = new BindingSource(dataSet, "Users");
             bindingSourceLoan = new BindingSource(bindingSourceOOO, "ooo-loanagr"); //bs with relations
             bindingSourceUsers = new BindingSource(bindingSourceLoan, "loanagr-users"); 
             bindingSourceFiles = new BindingSource(bindingSourceUsers, "users-files");
+
             gridOOO = new DataGridView(); //dg OOO
             gridOOO.Size = new Size(300, 610);
             gridOOO.Location = new Point(5, 5);
             gridOOO.DataSource = bindingSourceOOO;
             gridLoan = new DataGridView(); //dg LoanAgr
-            gridLoan.Size = new Size(670, 150);
+            gridLoan.Size = new Size(250, 200);
             gridLoan.Location = new Point(310, 5);
             gridLoan.DataSource = bindingSourceLoan;
             gridUsers = new DataGridView(); //dg Users
-            gridUsers.Size = new Size(670, 200);
-            gridUsers.Location = new Point(310, gridLoan.Bottom + 30);
+            gridUsers.Size = new Size(350, 200);
+            gridUsers.Location = new Point(570, 5);
             gridUsers.DataSource = bindingSourceUsers;
             gridFiles = new DataGridView(); //dg Files
             gridFiles.Size = new Size(670, 200);
             gridFiles.Location = new Point(310, gridUsers.Bottom + 30); 
             gridFiles.DataSource = bindingSourceFiles;
+
             this.Controls.AddRange(new Control[] { gridUsers, gridFiles, gridOOO, gridLoan }); //control with 4 dg
-            //      dataSet.Tables["Users"].Columns["Id"].ReadOnly = true; // R/O for 3 id in tables (temporary security, in future hide ID's in SELECT query)
-            dataSet.Tables["OOO"].Columns["IdOOO"].ReadOnly = true;
-            //      dataSet.Tables["Files"].Columns["UsersId"].ReadOnly = true;
-            //      dataSet.Tables["Files"].Columns["UsersId"].ReadOnly = true;
+
+            dataSet.Tables["OOO"].Columns["IdOOO"].ColumnMapping = MappingType.Hidden; // hidden ID's 
+            dataSet.Tables["LoanAgr"].Columns["Id"].ColumnMapping = MappingType.Hidden;
+            dataSet.Tables["LoanAgr"].Columns["LoanId"].ColumnMapping = MappingType.Hidden;
+            dataSet.Tables["Users"].Columns["FILESId"].ColumnMapping = MappingType.Hidden;
+            dataSet.Tables["Users"].Columns["IdUsers"].ColumnMapping = MappingType.Hidden;
         }
 
-        private void saveButtonOOO_Click(object sender, EventArgs e) //save for OOO in DB - TO-DO
+        private void saveButtonOOO_Click(object sender, EventArgs e) //save for OOO
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
+            {
+                connection.Open();
+                adapterOOO = new SqlDataAdapter("SELECT * FROM OOO;", connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapterOOO);
+                adapterOOO.InsertCommand = new SqlCommand("sp_OOO", connection);
+                adapterOOO.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapterOOO.InsertCommand.Parameters.Add(new SqlParameter("@наименование", SqlDbType.NVarChar, 50, "Наименование"));
+                adapterOOO.InsertCommand.Parameters.Add(new SqlParameter("@принят", SqlDbType.Date, 30, "Принят"));
+                SqlParameter parameter = adapterOOO.InsertCommand.Parameters.Add("@IdOOO", SqlDbType.Int, 10, "IdOOO");
+                parameter.Direction = ParameterDirection.Output;
+                adapterOOO.Update(dataSet.Tables["OOO"]);
+            }
+        }
+        private void saveButtonLoan_Click(object sender, EventArgs e) //save for LoanAgr
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
+            {
+                connection.Open();
+                adapterLoan = new SqlDataAdapter("SELECT * FROM LoanAgr;", connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapterLoan);
+                adapterLoan.InsertCommand = new SqlCommand("sp_Loan", connection);
+                adapterLoan.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapterLoan.InsertCommand.Parameters.Add(new SqlParameter("@договор", SqlDbType.NVarChar, 50, "Договор"));
+                adapterLoan.InsertCommand.Parameters.Add(new SqlParameter("@принят", SqlDbType.Date, 30, "Принят"));
+                adapterLoan.InsertCommand.Parameters.Add(new SqlParameter("@loanid", SqlDbType.Int, 10, "LoanId"));
+                SqlParameter parameter = adapterLoan.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 10, "Id");
+
+                parameter.Direction = ParameterDirection.Output;
+                adapterLoan.Update(dataSet.Tables["LoanAgr"]);
+            }
+        }
+        private void saveButtonUsers_Click(object sender, EventArgs e) //save for Users
         {
             using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
             {
@@ -79,24 +120,8 @@ namespace NlgDBcredProg
                 adapterUsers.InsertCommand.CommandType = CommandType.StoredProcedure;
                 adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@name", SqlDbType.NText, 10, "Name"));
                 adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@age", SqlDbType.NText, 10, "Age"));
-                SqlParameter parameter = adapterUsers.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 10, "Id");
-                parameter.Direction = ParameterDirection.Output;
-                adapterUsers.Update(dataSet.Tables["Users"]);
-            }
-        }
-        private void saveButtonUsers_Click(object sender, EventArgs e) //save for Users in DB - WORK
-        {
-            using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
-            {
-                connection.Open(); 
-                adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection);
-                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapterUsers);
-                adapterUsers.InsertCommand = new SqlCommand("sp_Users", connection);
-                adapterUsers.InsertCommand.CommandType = CommandType.StoredProcedure;
-                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@name", SqlDbType.NText, 10, "Name"));
-                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@age", SqlDbType.NText, 10,"Age"));
                 adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@idusers", SqlDbType.Int, 10, "IdUsers"));
-                SqlParameter parameter = adapterUsers.InsertCommand.Parameters.Add("@FILESId", SqlDbType.Int, 10,"FILESId");
+                SqlParameter parameter = adapterUsers.InsertCommand.Parameters.Add("@FILESId", SqlDbType.Int, 10, "FILESId");
                 parameter.Direction = ParameterDirection.Output;
                 adapterUsers.Update(dataSet.Tables["Users"]);
             }
@@ -105,40 +130,16 @@ namespace NlgDBcredProg
         {
             using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
             {
-                connection.Open();
-                adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection);
-                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapterUsers);
-                adapterUsers.InsertCommand = new SqlCommand("sp_Users", connection);
-                adapterUsers.InsertCommand.CommandType = CommandType.StoredProcedure;
-                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@name", SqlDbType.NText, 10, "Name"));
-                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@age", SqlDbType.NText, 10, "Age"));
-                SqlParameter parameter = adapterUsers.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 10, "Id");
-                parameter.Direction = ParameterDirection.Output;
-                adapterUsers.Update(dataSet.Tables["Users"]);
+
             }
         }
-        private void saveButtonLoan_Click(object sender, EventArgs e) //save for LoanAgr in DB - TO-DO
-        {
-            using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=usersdb;Integrated Security=True"))
-            {
-                connection.Open();
-                adapterUsers = new SqlDataAdapter("SELECT * FROM Users;", connection);
-                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapterUsers);
-                adapterUsers.InsertCommand = new SqlCommand("sp_Users", connection);
-                adapterUsers.InsertCommand.CommandType = CommandType.StoredProcedure;
-                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@name", SqlDbType.NText, 10, "Name"));
-                adapterUsers.InsertCommand.Parameters.Add(new SqlParameter("@age", SqlDbType.NText, 10, "Age"));
-                SqlParameter parameter = adapterUsers.InsertCommand.Parameters.Add("@FILESId", SqlDbType.Int, 10, "FILESId");
-                parameter.Direction = ParameterDirection.Output;
-                adapterUsers.Update(dataSet.Tables["Users"]);
-            }
-        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
             StartPosition = FormStartPosition.CenterScreen; //form position and size
             this.Left += 400;
-            Size = new Size(1000, 700); 
+            Size = new Size(1000, 800); 
         }
 
     }
