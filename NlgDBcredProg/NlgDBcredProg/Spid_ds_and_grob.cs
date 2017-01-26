844d20acbdceaac9454aa90d857fb3ddf45ddf1b
gmaksim <gmaksim@users.noreply.github.com>
2017-01-26 20:27:51 +0300

    + 2 buttons + little form fixes and work in DB

--- NlgDBcredProg/NlgDBcredProg/Spid_ds_and_grob.cs
+++ NlgDBcredProg/NlgDBcredProg/Spid_ds_and_grob.cs
@@ -13,7 +13,7 @@ namespace NlgDBcredProg
         DataSet dataSet;
         SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS; Initial Catalog=CredDogCIB; Integrated Security=True");
         SqlDataAdapter adZalogPoruch,adSpDSZalPor, adGrpObject, adDopSogZalPor, adObjData;
-        BindingSource bsSpDSZalPor, bsGrpObject, bsDopSogZalPor, bsObjData;
+        BindingSource bsZalogPoruch, bsSpDSZalPor, bsGrpObject, bsDopSogZalPor, bsObjData;
         DataGridView gdSpDSZalPor, gdGrpObject, gdDopSogZalPor, gdObjData;
 
         public Spid_ds_and_grob()
@@ -35,19 +35,26 @@ namespace NlgDBcredProg
 
 
             dataSet = new DataSet();
+            adZalogPoruch.Fill(dataSet, "ZalogPoruch");
             adSpDSZalPor.Fill(dataSet, "SpDSZalPor");
             adGrpObject.Fill(dataSet, "GrpObject");
             adDopSogZalPor.Fill(dataSet, "DopSogZalPor");
             adObjData.Fill(dataSet, "ObjData");
 
-            dataSet.Relations.Add("SpDSZalPor-DopSogZalPor", dataSet.Tables["SpDSZalPor"].Columns["idSpDSZP"], dataSet.Tables["DopSogZalPor"].Columns["id"]);
+
+            dataSet.Relations.Add("ZalogPoruch-SpDSZalPor", dataSet.Tables["ZalogPoruch"].Columns["idZalPor"], dataSet.Tables["SpDSZalPor"].Columns["id"]);
+            dataSet.Relations.Add("ZalogPoruch-GrpObject", dataSet.Tables["ZalogPoruch"].Columns["idZalPor"], dataSet.Tables["GrpObject"].Columns["id"]);
+            dataSet.Relations.Add("SpDSZalPor-DopSogZalPor", dataSet.Tables["SpDSZalPor"].Columns["idSpDSZP"], dataSet.Tables["DopSogZalPor"].Columns["id"], false);
             dataSet.Relations.Add("GrpObject-ObjData", dataSet.Tables["GrpObject"].Columns["idGrObj"], dataSet.Tables["ObjData"].Columns["id"]);
 
+            bsZalogPoruch = new BindingSource(dataSet, "ZalogPoruch");
             bsSpDSZalPor = new BindingSource(dataSet, "SpDSZalPor");
             bsGrpObject = new BindingSource(dataSet, "GrpObject");
             bsDopSogZalPor = new BindingSource(dataSet, "DopSogZalPor");
             bsObjData = new BindingSource(dataSet, "ObjData");
 
+            bsSpDSZalPor = new BindingSource(bsZalogPoruch, "ZalogPoruch-SpDSZalPor");
+            bsGrpObject = new BindingSource(bsZalogPoruch, "ZalogPoruch-GrpObject");
             bsDopSogZalPor = new BindingSource(bsSpDSZalPor, "SpDSZalPor-DopSogZalPor");
             bsObjData = new BindingSource(bsGrpObject, "GrpObject-ObjData");
 
@@ -61,21 +68,21 @@ namespace NlgDBcredProg
             gdDopSogZalPor.DataSource = bsDopSogZalPor;
             gdGrpObject = new DataGridView(); //dg GrpObject
             gdGrpObject.Size = new Size(250, 250);
-            gdGrpObject.Location = new Point(5, gdSpDSZalPor.Bottom + 10);
+            gdGrpObject.Location = new Point(5, gdSpDSZalPor.Bottom + 50);
             gdGrpObject.DataSource = bsGrpObject;
             gdObjData = new DataGridView(); //dg ObjData
             gdObjData.Size = new Size(600, 250);
-            gdObjData.Location = new Point(275, gdDopSogZalPor.Bottom + 10);
+            gdObjData.Location = new Point(275, gdDopSogZalPor.Bottom + 50);
             gdObjData.DataSource = bsObjData;
 
             this.Controls.AddRange(new Control[] {  gdSpDSZalPor, gdGrpObject, gdDopSogZalPor, gdObjData });
 
-            dataSet.Tables["SpDSZalPor"].Columns["id"].ColumnMapping = MappingType.Hidden;
-            dataSet.Tables["SpDSZalPor"].Columns["idSpDSZP"].ColumnMapping = MappingType.Hidden;
-            dataSet.Tables["DopSogZalPor"].Columns["id"].ColumnMapping = MappingType.Hidden;
-            dataSet.Tables["GrpObject"].Columns["id"].ColumnMapping = MappingType.Hidden;
-            dataSet.Tables["ObjData"].Columns["id"].ColumnMapping = MappingType.Hidden;
-            dataSet.Tables["GrpObject"].Columns["idGrObj"].ColumnMapping = MappingType.Hidden;
+           // dataSet.Tables["SpDSZalPor"].Columns["id"].ColumnMapping = MappingType.Hidden;
+         //   dataSet.Tables["SpDSZalPor"].Columns["idSpDSZP"].ColumnMapping = MappingType.Hidden;
+         //   dataSet.Tables["DopSogZalPor"].Columns["id"].ColumnMapping = MappingType.Hidden;
+         //   dataSet.Tables["GrpObject"].Columns["id"].ColumnMapping = MappingType.Hidden;
+          //  dataSet.Tables["ObjData"].Columns["id"].ColumnMapping = MappingType.Hidden;
+         //   dataSet.Tables["GrpObject"].Columns["idGrObj"].ColumnMapping = MappingType.Hidden;
 
         }
 
@@ -85,5 +92,45 @@ namespace NlgDBcredProg
             this.Left += 400;
             Size = new Size(900, 700);
         }
+
+
+
+        private void saveSpDSZalPor_Click(object sender, EventArgs e) //save for SpDSZalPor
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=CredDogCIB;Integrated Security=True"))
+            {
+                connection.Open();
+                adSpDSZalPor = new SqlDataAdapter("SELECT * FROM SpDSZalPor;", connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adSpDSZalPor);
+                adSpDSZalPor.InsertCommand = new SqlCommand("sp_SpDSZalPor", connection);
+                adSpDSZalPor.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adSpDSZalPor.InsertCommand.Parameters.Add(new SqlParameter("@Доп_соглашение", SqlDbType.NVarChar, 50, "Доп_соглашение"));
+                adSpDSZalPor.InsertCommand.Parameters.Add(new SqlParameter("@Принято", SqlDbType.Date, 30, "Принято"));
+                adSpDSZalPor.InsertCommand.Parameters.Add(new SqlParameter("@id", SqlDbType.Int, 30, "id"));
+                SqlParameter parameter = adSpDSZalPor.InsertCommand.Parameters.Add("@idSpDSZP", SqlDbType.Int, 10, "idSpDSZP");
+                parameter.Direction = ParameterDirection.Output;
+                adSpDSZalPor.Update(dataSet.Tables["SpDSZalPor"]);
+            }
+        }
+
+        private void saveDopSogZalPor_Click(object sender, EventArgs e) //save for DopSogZalPor
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=.\cibEXPRESS;Initial Catalog=CredDogCIB;Integrated Security=True"))
+            {
+                connection.Open();
+                adDopSogZalPor = new SqlDataAdapter("SELECT * FROM DopSogZalPor;", connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adDopSogZalPor);
+                adDopSogZalPor.InsertCommand = new SqlCommand("sp_DopSogZalPor", connection);
+                adDopSogZalPor.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adDopSogZalPor.InsertCommand.Parameters.Add(new SqlParameter("@id", SqlDbType.Int, 10, "id"));
+                adDopSogZalPor.InsertCommand.Parameters.Add(new SqlParameter("@Дог_поруч_залога", SqlDbType.NVarChar, 300, "Дог_поруч_залога"));
+                adDopSogZalPor.InsertCommand.Parameters.Add(new SqlParameter("@Одобрение_сделки", SqlDbType.NVarChar, 300, "Одобрение_сделки"));
+                adDopSogZalPor.InsertCommand.Parameters.Add(new SqlParameter("@ЕГРЮЛ_на_дату_подп", SqlDbType.NVarChar, 300, "ЕГРЮЛ_на_дату_подп"));
+                adDopSogZalPor.InsertCommand.Parameters.Add(new SqlParameter("@Список_участн_на_дату", SqlDbType.NVarChar, 300, "Список_участн_на_дату"));
+                adDopSogZalPor.InsertCommand.Parameters.Add(new SqlParameter("@Согласие_на_обремен", SqlDbType.NVarChar, 300, "Согласие_на_обремен"));
+                adDopSogZalPor.Update(dataSet.Tables["DopSogZalPor"]);
+            }
+        }
+
     }
 }
