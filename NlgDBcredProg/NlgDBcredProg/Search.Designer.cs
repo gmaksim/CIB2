a4ffa384bf4ecbc6da93a7bf207f01c7132694fd
gmaksim <gmaksim@users.noreply.github.com>
2017-04-18 22:04:15 +0300

    hide password + fixed size

--- NlgDBcredProg/NlgDBcredProg/Search.Designer.cs
+++ NlgDBcredProg/NlgDBcredProg/Search.Designer.cs
@@ -86,6 +86,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Search";
             this.Text = "CIB - Поиск";
--- NlgDBcredProg/NlgDBcredProg/Spid_ds_and_grob.Designer.cs
+++ NlgDBcredProg/NlgDBcredProg/Spid_ds_and_grob.Designer.cs
@@ -112,6 +112,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Spid_ds_and_grob";
             this.Text = "CIB - Список доп.согл. и групп объектов";
